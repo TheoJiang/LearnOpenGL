10bd834db38305a965ab68cac4d81d16e642e46c
JiangKai <kevlislyon@live.com>
2020-06-13 01:19:11 +0800

    Render a triangle

--- LearnOpenGL/HelloWindow.cpp
+++ LearnOpenGL/HelloWindow.cpp
@@ -1,12 +1,36 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "Triangle.h"
 
 using namespace std;
 
 void framebuffer_size_callback(GLFWwindow * win, int width, int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+int key = GLFW_KEY_UNKNOWN;
+// process all input, query GLFW whether relevant keys are pressed/release this frame and react accordingly
+void processInput(GLFWwindow *win)
+{
+	if (glfwGetKey(win, GLFW_KEY_ESCAPE))
+	{
+		glfwSetWindowShouldClose(win, true);
+	}
+	
+	if (glfwGetKey(win, GLFW_KEY_1) == GLFW_PRESS)
+	{
+		key = GLFW_KEY_1;		
+	}
+
+	if (key == GLFW_KEY_1)
+	{
+		DrawTriangle();
+	}
+
+	// 双缓冲， 交换缓冲区
+	glfwSwapBuffers(win);
 }
 
 int main()
@@ -25,6 +49,7 @@ int main()
 
 	glfwMakeContextCurrent(win);
 
+	// Load All OpenGL Function pointers
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Init GLAD Error" << std::endl;
@@ -33,15 +58,27 @@ int main()
 
 	glViewport(0, 0, 400, 300);
 
+	// Trigger when Window Size changed
 	glfwSetFramebufferSizeCallback(win, framebuffer_size_callback);
 
+	GenerateTriangle();
 	while (!glfwWindowShouldClose(win))
 	{
-		glfwSwapBuffers(win);
-		glfwPollEvents();
+		// Process Input Commands
+		processInput(win);
+
+		// Process Rendering Commands
+		// X: This a State-using Function		√: This a State-Setting/Changing function
+		glClearColor(.2, .3, .3, 1);
+		// X: This a State-Changing Function		√: This a State-using function, it uses the current state to retrieve/find the clearing color from.
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// 触发按键事件					
+		glfwPollEvents();		
 	}
 
 	glfwTerminate();
+
 	return 0;
 }
 
---
+++ LearnOpenGL/Triangle.cpp
@@ -0,0 +1,126 @@
+#include "Triangle.h"
+#include <string>
+using namespace std;
+
+float vertices[] = {
+	-0.5, -0.5, 0.0,
+	0.5, -0.5, 0.0,
+	0.0, 0.5, 0.0
+};
+
+unsigned int vs;
+unsigned int fs;
+unsigned int sProgram;
+
+void CreateAndCompileShader(/*int &vs, int &fs*/)
+{
+	string vss =
+		"#version 330 core                                          \n"
+		"layout (location = 0) in vec3 aPos;                      \n"
+		"void main(){                                             \n"
+		"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);}    \n";
+
+	const char *vsc_str = vss.c_str();
+	vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vsc_str, NULL);
+	glCompileShader(vs);
+
+	// check suc
+	int suc;
+	char infoLog[512];
+	// the iv of glGetShaderiv means a vector of ints
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &suc);
+	if (!suc)
+	{
+		glGetShaderInfoLog(vs, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	string fss =
+		"#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"		FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n";
+
+	const char *fsc_str = fss.c_str();
+	fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fsc_str, NULL);
+	glCompileShader(fs);
+
+	// check suc
+	glGetShaderiv(fs, GL_COMPILE_STATUS, &suc);
+	if (!suc)
+	{
+		glGetShaderInfoLog(fs, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// Link Shader
+	sProgram = glCreateProgram();
+
+	glAttachShader(sProgram, vs);
+	glAttachShader(sProgram, fs);
+	glLinkProgram(sProgram);
+
+	glGetProgramiv(sProgram, GL_LINK_STATUS, &suc);
+	if (!suc) {
+		glGetProgramInfoLog(sProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	glUseProgram(sProgram);
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+}
+
+unsigned int VBO;
+unsigned int VAO;
+void GenerateTriangle()
+{
+	//// use buffer id to generate a Buffer Object
+	//glGenBuffers(1, &VBO);
+	//// bind the buffer object VBO to GL_ARRARY_TARGET, it allows us to send lots data into memory
+	//glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//// cause the buffer object was bound, the buffer calls subsequence will configure this buffer object
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	//-----------------
+	// VAO
+	glGenVertexArrays(1, &VAO);
+	// Bind VAO
+	glBindVertexArray(VAO);
+	// Bind VBO
+	// Copy VA to Buffer
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//-----------------
+
+
+	// vertext attribute pointer
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	CreateAndCompileShader();
+}
+
+void DrawTriangle()
+{
+	//glUseProgram(sProgram);
+
+
+	// Q: why one more time？
+	// A: 正常有多个VAO时要切换绑定, 此处只有一个, 所以不用每次都去绑定, 因为不需要切换绑定.
+	//glBindVertexArray(VAO);
+
+	// Draw
+	//glDrawArrays(GL_POINTS, 0, 3);
+	//glDrawArrays(GL_LINE_LOOP, 0, 3);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	// 不用每次都解绑
+	// glBindVertexArray(0); 
+}
+
